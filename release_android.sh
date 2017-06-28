b5f7184f159dd30c6e0c4748f4a7c44c0eda1fba
Rodolfo Silva <contato@rodolfosilva.com>
2017-06-27 22:21:10 -0300

    Atualiza configurações e script de geração de releases

--- release_android.sh
+++ release_android.sh
@@ -22,8 +22,8 @@ then
   rm $releaseFile
 fi
 
-echo "ionic build android --release"
-ionic build android --release --$apkEnv
+echo "ionic cordova build android --release"
+ionic cordova build android --release --$apkEnv
 
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore key/devry-calc.keystore -storepass 0p9o8i7u -keypass 0p9o8i7u platforms/android/build/outputs/apk/android-release-unsigned.apk devry_calc
 jarsigner -verify -verbose -certs platforms/android/build/outputs/apk/android-release-unsigned.apk
