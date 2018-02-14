d0e4fe74403d38391c279c70e18314f8b632957c
Rodolfo Silva <contato@rodolfosilva.com>
2018-02-13 21:27:24 -0300

    Adicionando testes

--- src/reducers/disciplinas.js
+++ src/reducers/disciplinas.js
@@ -1,19 +1,9 @@
 import uuid from 'uuid/v4'
-import { parseObjectValuesToFloat } from '../utils/parseObjectValuesToFloat'
 import omit from 'lodash/omit'
 import { SAVE_DISCIPLINA, ADD_DISCIPLINA, REMOVE_DISCIPLINA } from '../constants/ActionTypes'
+import { sanitizeDisciplina } from '../utils'
 
 const initialState = {}
-
-export const sanitizeDisciplina = (id, { titulo, notas }) => {
-  return {
-    [id]: {
-      id,
-      titulo,
-      notas: parseObjectValuesToFloat({ ap1: null, ap2: null, ap3: null, ...notas })
-    }
-  }
-}
 
 const disciplinas = (state = initialState, action) => {
   switch (action.type) {
